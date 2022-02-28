4be25934b5a4ed20f464056a0ac28a2886a69921
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-02-28 10:29:01 +0100

    binding and subobjects

--- w2/d1/oop.js
+++ w2/d1/oop.js
@@ -10,7 +10,13 @@ const bike = {
   owner: 'Abdou',
   tyres: [
     { position: 'front', status: 'inflated' },
-    { position: 'back', status: 'punctured' },
+    {
+      position: 'back',
+      status: 'punctured',
+      repair() {
+        this.status = 'inflated'
+      },
+    },
   ],
   speedInKmph: 0,
   speedUp(increaseInSpeed) {
@@ -40,3 +46,31 @@ console.log(bike.speedInKmph)
 
 console.log(bike.getDescription())
 console.log(bike.getOwnerName())
+
+function callSomething(callback) {
+  return callback()
+}
+
+callSomething(() => console.log('hello'))
+// we're passing the function expression of getDescription
+// but we lose the reference to the object itself
+// this means that 'this' is not the bike
+const result = callSomething(bike.getDescription)
+console.log(result)
+
+// Two ways to pass object methods as an argument
+// WITHOUT losing the 'this' context
+
+// a new function - easiest is to create an arrow
+const result2 = callSomething(() => bike.getDescription())
+console.log('arrow function', result2)
+
+// create a bound function
+const result3 = callSomething(bike.getDescription.bind(bike))
+console.log('binding', result3)
+
+console.log(this)
+
+console.log(bike.tyres)
+bike.tyres[1].repair()
+console.log(bike.tyres)
