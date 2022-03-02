233d70a43909b5e80f9f723b343e08bb10db6232
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-03-02 11:11:10 +0100

    week 2 day 3 timings

---
+++ w2/d3/main.js
@@ -0,0 +1,31 @@
+const clocks = document.querySelectorAll('p[data-hour]')
+
+const buildClock = (element) => {
+  const targetHour = element.dataset.hour
+  const now = new Date(Date.now())
+  const target = new Date(now)
+  target.setHours(targetHour)
+  target.setMinutes(0)
+  target.setSeconds(0)
+
+  if (target < now) {
+    element.textContent = 'NOW'
+  } else {
+    setInterval(() => setTime(target, element), 500)
+  }
+}
+
+const setTime = (targetTime, element) => {
+  const millisecondsRemaining = targetTime - Date.now()
+
+  const totalSeconds = Math.ceil(millisecondsRemaining / 1000)
+  const totalMinutes = Math.ceil(totalSeconds / 60)
+  const seconds = (totalSeconds % 60).toString().padStart(2, '0')
+  const minutes = (totalMinutes % 60).toString().padStart(2, '0')
+  const hours = Math.floor(totalMinutes / 60)
+    .toString()
+    .padStart(2, '0')
+  element.textContent = `${hours}:${minutes}:${seconds}`
+}
+
+clocks.forEach(buildClock)
