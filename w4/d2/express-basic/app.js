b1464d5bdc8346dc9d1b8de4928d0a24786fde18
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-03-14 14:13:24 +0100

    about page from template

--- w4/d2/express-basic/app.js
+++ w4/d2/express-basic/app.js
@@ -4,6 +4,10 @@ const express = require('express')
 // Express server will be handling requests and responses
 const app = express()
 
+// We set the "view engine" to say that handlebars will
+// pre-process html responses
+app.set('view engine', 'hbs')
+
 // Static files
 app.use(express.static('public'))
 
@@ -13,9 +17,20 @@ app.get('/home', (request, response, next) => {
   response.send('<h1>Our first express app</h1>')
 })
 
+// responding with a static html file
 app.get('/friend', (request, response, next) =>
   response.sendFile(__dirname + '/views/neighbour.html')
 )
 
+// responding with a dynamic handlebars template
+app.get('/about', (request, response, next) => {
+  const data = {
+    title: 'About',
+    author: 'Robin James Kerrison',
+  }
+
+  response.render('template', data)
+})
+
 // Server Started
-app.listen(3000, () => console.log('My first app listening on port 3000! '))
+app.listen(3000, () => console.log('My first app listening on port 3000!'))
