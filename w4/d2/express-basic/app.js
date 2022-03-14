9c6845bdc9657f1ad7af6f1b5705304eafe282f7
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-03-14 12:49:28 +0100

    express port 3000

--- w4/d2/express-basic/app.js
+++ w4/d2/express-basic/app.js
@@ -9,3 +9,6 @@ app.get('/home', (request, response, next) => {
   console.log(request)
   response.send('<h1>Our favourite bear</h1>')
 })
+
+// Server Started
+app.listen(3000, () => console.log('My first app listening on port 3000! '))
