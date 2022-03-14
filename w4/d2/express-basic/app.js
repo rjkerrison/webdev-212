04c02ca7f4f20dea7cb39f62fe04fdf831be2726
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-03-14 12:48:43 +0100

    home route

--- w4/d2/express-basic/app.js
+++ w4/d2/express-basic/app.js
@@ -3,3 +3,9 @@ const express = require('express')
 // We create our own server named app
 // Express server will be handling requests and responses
 const app = express()
+
+// our first Route
+app.get('/home', (request, response, next) => {
+  console.log(request)
+  response.send('<h1>Our favourite bear</h1>')
+})
