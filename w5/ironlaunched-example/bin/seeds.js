b933f65f6663965d03f2fa630c5f673e0926c779
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-03-21 17:53:29 +0100

    Review db seeding

--- w5/ironlaunched-example/bin/seeds.js
+++ w5/ironlaunched-example/bin/seeds.js
@@ -2,6 +2,7 @@
 
 const mongoose = require('mongoose')
 const Film = require('../models/Film.model')
+const Review = require('../models/Review.model')
 
 const MONGO_URI =
   process.env.MONGODB_URI || 'mongodb://localhost/ironlaunched-example'
@@ -42,12 +43,31 @@ const films = [
 // This is useful for testing our seeding, but we don't always want to do it
 Film.deleteMany()
   .then(() => {
+    return Review.deleteMany()
+  })
+  .then(() => {
     return Film.create(films)
   })
   .then((filmsFromDB) => {
     console.log(`Created ${filmsFromDB.length} films`)
+    return filmsFromDB
+  })
 
-    // Once created, close the DB connection
+  .then((films) => {
+    return Review.create([
+      {
+        rating: 4,
+        content: 'I LOVED IT but not 5-star loved it',
+        film: films[0].id,
+      },
+    ])
+  })
+  .then((reviews) => {
+    console.log(`Created ${reviews.length} reviews`)
+    return reviews
+  })
+  .then(() => {
+    // Once finished, close the DB connection
     return mongoose.connection.close()
   })
   .catch((err) =>
