68b6675ae4b1dff9bf423d5c8fe9a2c0e1262465
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-03-22 10:08:13 +0100

    populating film field manually

--- w5/ironlaunched-example/routes/reviews.routes.js
+++ w5/ironlaunched-example/routes/reviews.routes.js
@@ -1,9 +1,15 @@
+const Film = require('../models/Film.model.js')
 const Review = require('../models/Review.model.js')
 
 const router = require('express').Router()
 
 router.get('/', async (req, res, next) => {
   const reviews = await Review.find()
+
+  for (let review of reviews) {
+    const filmId = review.film
+    review.film = await Film.findById(filmId)
+  }
 
   res.render('reviews', { reviews })
 })
