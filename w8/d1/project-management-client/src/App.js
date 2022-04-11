bf6093c95e0dc6eb669e67c556e0eb00e5149844
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-04-11 11:45:44 +0200

    Navbar

--- w8/d1/project-management-client/src/App.js
+++ w8/d1/project-management-client/src/App.js
@@ -1,8 +1,19 @@
-// App.js
-
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  )
 }
+
 export default App
---
+++ w8/d1/project-management-client/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+      <p>I am a Navbar</p>
+      <Link to="/">Home</Link>
+    </div>
+  )
+}
+
+export default Navbar
