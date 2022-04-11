9f3aac4c46d08a84cd19dea5f547b0304c9b515e
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-04-11 10:51:17 +0200

    GET /api/projects/:projectId

--- w8/d1/project-management-server/routes/projects.routes.js
+++ w8/d1/project-management-server/routes/projects.routes.js
@@ -17,13 +17,23 @@ router.post('/projects', async (req, res, next) => {
 router.get('/projects', async (req, res, next) => {
   try {
     const projects = await Project.find()
-    // const tasks = await Task.find()
 
     for (let project of projects) {
       project._doc.tasks = await Task.find({ project: project._id })
     }
 
     res.json(projects)
+  } catch (err) {
+    res.json(err)
+  }
+})
+
+router.get('/projects/:projectId', async (req, res, next) => {
+  try {
+    const project = await Project.findById(req.params.projectId)
+    project._doc.tasks = await Task.find({ project: project._id })
+
+    res.json(project)
   } catch (err) {
     res.json(err)
   }
