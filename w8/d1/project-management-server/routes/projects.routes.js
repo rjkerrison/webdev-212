1c4f1b4e1ab1f46d347686367e85a83e067da97e
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-04-11 10:49:20 +0200

    GET /api/projects

--- w8/d1/project-management-server/routes/projects.routes.js
+++ w8/d1/project-management-server/routes/projects.routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const Project = require('../models/Project.model')
+const Task = require('../models/Task.model')
 
 //  POST /api/projects  -  Creates a new project
 router.post('/projects', async (req, res, next) => {
@@ -8,6 +9,21 @@ router.post('/projects', async (req, res, next) => {
   try {
     const response = await Project.create({ title, description })
     res.json(response)
+  } catch (err) {
+    res.json(err)
+  }
+})
+
+router.get('/projects', async (req, res, next) => {
+  try {
+    const projects = await Project.find()
+    // const tasks = await Task.find()
+
+    for (let project of projects) {
+      project._doc.tasks = await Task.find({ project: project._id })
+    }
+
+    res.json(projects)
   } catch (err) {
     res.json(err)
   }
